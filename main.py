e602dae4d801463675bf9d8f32a5ff9b0a7ed706
Michael Goldberg <deutschman1999@mail.ru>
2022-12-03 14:17:21 +0300

    ADD вывод тестового трека по запросу

--- main.py
+++ main.py
@@ -1,25 +1,54 @@
-import logging
-import os
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import Message
 from dotenv import load_dotenv
-from aiogram import Bot, Dispatcher, executor, types
+import os
+
+from aiogram.dispatcher import FSMContext
+from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager, StartMode
+from aiogram_dialog.widgets.kbd import Button
+from aiogram_dialog.widgets.text import Const
+from aiogram.types import CallbackQuery
+import requests
 
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 API_TOKEN = os.environ.get("API_TOKEN")
+CHANNEL_NAME = os.environ.get("CHANNEL_NAME")
+test_message = "Hello bitchessssss!"
 
-# Configure logging
-logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
+storage = MemoryStorage()
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher(bot, storage=storage)
+registry = DialogRegistry(dp)
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm AndanteBot!\nPowered by aiogram.")
+
+class States(StatesGroup):
+    main = State()
+    track = State()
+
+async def track_name(c: CallbackQuery, button: Button, manager: DialogManager):
+    await States.track.set()
+    await c.message.reply("Введите название трека: ")
+
+@dp.message_handler(state=States.track)
+async def send_message(message: Message, state: FSMContext):
+    await bot.send_audio(message.from_user.id, open(os.path.join(BASE_DIR, f'{message.text}.mp3'), "rb"), performer = "Unknown", title = "Unknown")
+
+main_window = Window(
+    Const("Вы хотите найти трек?"),
+    Button(Const("Да"), id="sender", on_click=track_name),
+    state=States.main,
+)
+
+@dp.message_handler(commands=["start"])
+async def start(m: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(States.main, mode=StartMode.RESET_STACK)
+
+dialog = Dialog(main_window)
+registry.register(dialog)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
